1ed6ce1e3d82368adeea4ba3bcae880bc6d63ac0
ItayZemah <itayzemah@users.noreply.github.com>
2021-04-26 21:53:00 +0300

    first commit

---
+++ Assets/Scripts/DoorMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMovement : MonoBehaviour
+{
+    private Animator anim;
+    private AudioSource doorOpenSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        doorOpenSound = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.SetBool("IsDoorOpen", true);
+        doorOpenSound.Play();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        anim.SetBool("IsDoorOpen", false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/DoorSideMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSideMove : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.SetBool("doorOpen", true);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        anim.SetBool("doorOpen", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
